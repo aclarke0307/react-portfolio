c5d9508fade8737b8e91ec133f0222a957e34c47
aclar <a.clarke0307@gmail.com>
2021-11-07 22:39:46 -0500

    add links to projects and update contactf form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -35,7 +35,7 @@ function ContactForm() {
 
   return (
     <section>
-      <h1 data-testid="h1tag">Contact me</h1>
+      <h1 data-testid="h1tag">Let's get in Touch!</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { capitalizeFirstLetter } from '../../utils/helpers';
 import Runbuddy from "../../assets/images/run-buddy.png";
 import Horiseon from "../../assets/images/horiseon.png";
 import Decideeat from "../../assets/images/decide-eat.png";
@@ -8,34 +7,42 @@ import Pricetracker from "../../assets/images/price-tracker.png";
 
 function Project() {
     return(
-        <section id="My-Projects" class="work" >
+        <section id="Projects" class="work" >
             <div>
                <h2 class="section-title">Projects</h2>
             </div>
         <div class="portfolio flex-container">
             <div className='flex-item'>
+            <a href= "https://aclarke0307.github.io/run-buddy/">
                 <img src={Runbuddy}
                 alt="Run Buddy"
                 className="portfolio-img"
                 />
+            </a>
             </div>
             <div className='flex-item'>
+            <a href="https://aclarke0307.github.io/horiseon/">
                 <img src={Horiseon}
                 alt="Horiseon"
                 className="portfolio-img"
                 />
+            </a>
             </div>
             <div className='flex-item'>
+            <a href="https://padredilg.github.io/decide-eat/">
                 <img src={Decideeat}
                 alt="Decide eat"
                 className="portfolio-img"
                 />
+            </a>
             </div>
             <div className='flex-item'>
+            <a href="https://fathomless-beach-31374.herokuapp.com/">
                 <img src={Pricetracker}
                 alt="Price Tracker"
                 className="portfolio-img"
                 />
+            </a>
             </div>
         </div>
         </section>
